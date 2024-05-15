d9febbe7fb2f753921461000258b788eda8b43c7
Carlos Galindo <utm22030587@utma.edu.mx>
2024-05-14 23:14:05 -0600

    style: change font size and text

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,11 +5,11 @@ const Body = () => {
         <div className="flex flex-col py-10 justify-center items-center">
             <h1 className="font-bold text-6xl text-center">Hi, I am Carlos Galindo</h1>
             <img src="./me.jpeg" alt="Me" className="text-center rounded-3xl py-3"  width="150px"/>
-            <h3 className="py-3"><span className="font-bold">Name: </span>Carlos Alejandro Galindo Islas</h3>
-            <h3 className="py-3"><span className="font-bold">Major: </span>IT Software Deevelopment</h3>
-            <h3 className="py-3"><span className="font-bold">Level: </span>6 term</h3>
-            <h3 className="py-3"><span className="font-bold">ID: </span>UTM22030587</h3>
-            <h3 className="py-3"><span className="font-bold">Email: </span>utm22030587@utma.edu.mx</h3>
+            <h3 className="py-3 text-lg"><span className="font-bold text-xl">Name: </span>Carlos Alejandro Galindo Islas</h3>
+            <h3 className="py-3 text-lg"><span className="font-bold text-xl">Major: </span>IT Software Development</h3>
+            <h3 className="py-3 text-lg"><span className="font-bold text-xl">Level: </span>6 term</h3>
+            <h3 className="py-3 text-lg"><span className="font-bold text-xl">ID: </span>UTM22030587</h3>
+            <h3 className="py-3 text-lg"><span className="font-bold text-xl">Email: </span>utm22030587@utma.edu.mx</h3>
         </div>
     </>
   )
